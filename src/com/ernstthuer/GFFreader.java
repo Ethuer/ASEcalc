390778c3ff24d6a0af3b1ea52e603a362d5d6bab
ethur <thuer.ernst@gmail.com>
2016-07-18 12:36:44 +0200

    Primitive description parser for CGD and ensembl

--- src/com/ernstthuer/GFFreader.java
+++ src/com/ernstthuer/GFFreader.java
@@ -15,16 +15,19 @@ public class GFFreader {
 
     /**
      * GFF files are diverse and parsing should implement a Sniffer, or preset choices.
-     *
-     *
      */
 
-    public enum feature {GENE,EXON,CDS,TRANSCRIPT}
+    public enum feature {
+        GENE, EXON, CDS, TRANSCRIPT
+    }
+
     private String direction;
 
     private static String[] lineList;
     private String feature;
     private static List<Gene> geneList;
+    private static String type = null;
+
 
 
     public GFFreader(String direction, String feature) {
@@ -33,7 +36,7 @@ public class GFFreader {
 
         try {
             this.lineList = openGFF(direction);
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("GFF file not found");
             System.out.println(e.getStackTrace());
         }
@@ -41,62 +44,97 @@ public class GFFreader {
 
     }
 
-    public String[] openGFF(String direction) throws IOException{
+    public String[] openGFF(String direction) throws IOException {
 
         FileReader reader = new FileReader(direction);
         BufferedReader bufferedReader = new BufferedReader(reader);
         int numberOfLines = countLines(bufferedReader);
         String[] linesOfFeatures = new String[numberOfLines];
 
-        for (int i=0; i < numberOfLines; i++) {
-            linesOfFeatures[ i ] = bufferedReader.readLine();
+        for (int i = 0; i < numberOfLines; i++) {
+            linesOfFeatures[i] = bufferedReader.readLine();
         }
-        bufferedReader.close( );
+        bufferedReader.close();
         return linesOfFeatures;
 
     }
 
-    public int countLines (BufferedReader reader) throws IOException {
+    public int countLines(BufferedReader reader) throws IOException {
 
-        String hasLine ;
+        String hasLine;
         int numberOfLines = 0;
 
-        while((hasLine = reader.readLine()) != null){
-            numberOfLines ++;
+        while ((hasLine = reader.readLine()) != null) {
+            numberOfLines++;
         }
         return numberOfLines;
 
     }
 
-    public List<Gene> geneList(String[] featureList){
+    public List<Gene> geneList(String[] featureList) {
         List<Gene> outList = null;
-        for(String entry: featureList){
+        for (String entry : featureList) {
             String[] row = entry.split("\t");
-            if(row[2].equals(this.feature)){
+            if (row[2].equals(this.feature)) {
                 // new feature here
                 int start = parseInt(row[3]);
                 int stop = parseInt(row[4]);
-                Gene newGene = new Gene(row[0],start,stop,row[8]);
+                Gene newGene = new Gene(row[0], start, stop, row[8]);
             }
         }
         return outList;
     }
 
 
+    public String descriptionParser(String fullDescription) {
 
-    public String descriptionParser(String fullDescription){
-
-        String featureID ;
+        String featureID;
         String[] desc = fullDescription.split(";");
 
         Pattern CGD = Pattern.compile("ID=*;");
         Pattern ENSEMBL = Pattern.compile("gene_id \"*\";");
         Pattern ENSEXP = Pattern.compile("hid=trf;");
 
-        for(String element : desc){
-            
+        // Simple sniffer
+        while (type == null) {
+            for (String element : desc) {
+                if (element.contains("ID")) {
+                    type = "CGD";
+                }
+                if (element.contains("gene_id")) {
+                    type = "ENSEMBL";
+                }
+                if (element.contains("gid")) {
+                    type = "ENSEXP";
+                }
+            }
         }
 
-    return null;
+        switch (type) {
+            case "CGD":
+                for(String element: desc){
+                    if(element.contains("ID")){
+                        featureID = element.split("=")[1];
+                        return featureID;
+                    }
+                }
+                break;
+
+            case "ESEMBL":
+                for(String element: desc){
+                    if(element.contains("_id")){
+                        featureID = element.split("\"")[1];
+                        return featureID;
+                    }
+                }
+                break;
+
+            case "ENSEXP":
+                break;
+
+
+        }
+        return null;
+
     }
 }
