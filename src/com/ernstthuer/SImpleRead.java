91261ba794810947587668a6f26bf09d1becd193
ethur <thuer.ernst@gmail.com>
2016-07-21 18:13:30 +0200

    create simple read class

---
+++ src/com/ernstthuer/SImpleRead.java
@@ -0,0 +1,14 @@
+package com.ernstthuer;
+
+/**
+ * Created by ethur on 7/21/16.
+ */
+public class SImpleRead {
+    private int start;
+    private String MZ;
+
+    public SImpleRead(int start, String MZ) {
+        this.start = start;
+        this.MZ = MZ;
+    }
+}
